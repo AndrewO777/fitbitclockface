b3a6a1f1ddfe0bb01195dd3d79c08eef152d8ecf
AndrewO777 <andrewodom777@gmail.com>
2023-03-11 01:52:10 -0500

    fix to clock face

--- app/index.js
+++ app/index.js
@@ -1,12 +1,44 @@
 import clock from "clock";
-import * as document from "document";
-import { preferences } from "user-settings";
-import * as util from "./common/utils";
-import { HeartRateSensor } from "heart-rate";
+import document from "document";
+
+
+/*import { preferences } from "user-settings";
+import * as util from "./common/utils";*/
+//import { HeartRateSensor } from "heart-rate";
 
 // Update the clock every minute
-clock.granularity = "minutes";
+clock.granularity = "seconds";
 
+let hourHand = document.getElementById("hours");
+let minHand = document.getElementById("mins");
+
+// Returns an angle (0-360) for the current hour in the day, including minutes
+function hoursToAngle(hours, minutes) {
+  let hourAngle = (360 / 12) * hours;
+  let minAngle = (360 / 12 / 60) * minutes;
+  return hourAngle + minAngle;
+}
+
+// Returns an angle (0-360) for minutes
+function minutesToAngle(minutes) {
+  return (360 / 60) * minutes;
+}
+
+// Rotate the hands every tick
+function updateClock() {
+  let today = new Date();
+  let hours = today.getHours() % 12;
+  let mins = today.getMinutes();
+
+  hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
+  minHand.groupTransform.rotate.angle = minutesToAngle(mins);
+}
+
+// Update the clock every tick event
+clock.addEventListener("tick", updateClock);
+
+
+/*
 // Get a handle on the <text> element
 const myLabel = document.getElementById("myLabel");
 //const hrmLabel = document.getElementById("hrmLabel");
@@ -33,4 +65,4 @@ clock.ontick = (evt) => {
 //    hrmLabel.text = `${hrm.heartRate}`;
 //  });
 //  hrm.start();
-//}
+//}*/
