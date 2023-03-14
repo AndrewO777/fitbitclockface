c77a2b48d24f01706c5e374d1c11f4d8901c9189
AndrewO777 <andrewodom777@gmail.com>
2023-03-14 18:34:07 -0400

    Fixed error on install of clockface, fixed zero padding on time, added background image transition

--- app/index.js
+++ app/index.js
@@ -1,46 +1,21 @@
 import clock from "clock";
-import * as document from "document";
+import document from "document";
 import { today as todayActivity } from 'user-activity';
 
-/*import { preferences } from "user-settings";
-import * as util from "./common/utils";*/
+import { preferences } from "user-settings";
+import * as util from "./common/utils";
 import { HeartRateSensor } from "heart-rate";
 
 const sElem = document.getElementById("stepsText");
-const myLabel = document.getElementById("clock-label");
+const clockLabel = document.getElementById("clock-label");
 const hrmLabel = document.getElementById("hrmLabel");
-// Update the clock every minute
-//clock.granularity = "seconds";
+const backgroundImage = document.getElementById("backgroundImage");
 
-//let hourHand = document.getElementById("hours");
-//let minHand = document.getElementById("mins");
+let images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png"];
+let imageIndex = Math.floor(Math.random() * 12);
+backgroundImage.href = images[imageIndex];
 
-//// Returns an angle (0-360) for the current hour in the day, including minutes
-//function hoursToAngle(hours, minutes) {
-//  let hourAngle = (360 / 12) * hours;
-//  let minAngle = (360 / 12 / 60) * minutes;
-//  return hourAngle + minAngle;
-//}
-
-//// Returns an angle (0-360) for minutes
-//function minutesToAngle(minutes) {
-//  return (360 / 60) * minutes;
-//}
-
-//// Rotate the hands every tick
-//function updateClock() {
-//  let today = new Date();
-//  let hours = today.getHours() % 12;
-//  let mins = today.getMinutes();
-
-//  hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
-//  minHand.groupTransform.rotate.angle = minutesToAngle(mins);
-//}
-
-//// Update the clock every tick event
-//clock.addEventListener("tick", updateClock);
-
-
+clock.granularity = 'minutes';
 
 // Get a handle on the <text> element
 
@@ -52,12 +27,23 @@ clock.ontick = (evt) => {
   if (preferences.clockDisplay === "12h") {
     // 12h format
     hours = hours % 12 || 12;
-  } else {
-    // 24h format
-    hours = util.zeroPad(hours);
   }
-  let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+  let mins = today.getMinutes();
+  if (hours >= 10)
+    clockLabel.text = `${hours}:`;
+  else
+    clockLabel.text = `0${hours}:`;
+  if (mins >= 10)
+    clockLabel.text += mins;
+  else
+    clockLabel.text += `0${mins}`;
+  if (mins === 0){
+    let newImageIndex = Math.floor(Math.random() * 12);
+    while (newImageIndex === imageIndex)
+      newImageIndex = Math.floor(Math.random() * 12);
+    imageIndex = newImageIndex;
+    backgroundImage.href=images[imageIndex];
+  }
 }
 
 if (HeartRateSensor) {
@@ -90,19 +76,3 @@ if (todayActivity.adjusted != null) {
 } else {
     sElem.text = "--";
 }
-
-
-
-//clock.granularity = "minutes"; // seconds, minutes, or hours
-
-//clock.addEventListener("tick", (evt) => {
-//    // tick every minute
-//});
-
-//clock.granularity = "seconds"; // seconds, minutes, hours
-
-//const clockLabel = document.getElementById("clock-label");
-
-//clock.addEventListener("tick", (evt) => {
-//    clockLabel.text = evt.date.toTimeString().slice(0, -4);
-//});
